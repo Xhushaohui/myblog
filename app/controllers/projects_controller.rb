9f9960f14c6470bd60bde0f532be9b5698470215
Xhushaohui <xshaohuihu@163.com>
2016-06-27 14:37:52 +0800

    Setup & styling basic project page

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,34 @@
+class ProjectsController < ApplicationController
+
+	before_action :find_project, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@projects = Project.all.order("Created_at desc").paginate(page: params[:page], per_page: 3)
+	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		@project = Project.new project_params
+
+		if @project.save
+			redirect_to @project
+		else
+			render 'new'
+		end
+	end
+
+
+private
+
+	def find_project
+		@project = Project.find(params[:id])
+	end
+
+	def project_params
+		params.require(:project).permit(:title, :description, :link)
+	end
+
+end
